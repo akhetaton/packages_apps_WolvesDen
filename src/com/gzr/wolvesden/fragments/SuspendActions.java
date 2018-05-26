ef56d536514a8c3976ab241d724fa79c7703a37f
Akhenaton <akhetaton@hotmail.it>
2018-05-26 18:15:51 -0400

    dioporcooooo

--- src/com/gzr/wolvesden/fragments/SuspendActions.java
+++ src/com/gzr/wolvesden/fragments/SuspendActions.java
@@ -148,7 +148,7 @@ public class SuspendActions extends SettingsPreferenceFragment implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.XTENDED;
+        return MetricsProto.MetricsEvent.Validus;
     }
 
     @Override
